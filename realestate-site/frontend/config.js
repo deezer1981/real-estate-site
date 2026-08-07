7475c14902683e19ef30da5ecfb507a0d7e3ee2d
deezer1981 <145301005+deezer1981@users.noreply.github.com>
2026-08-08 02:53:07 +0330

    Update config.js

--- realestate-site/frontend/config.js
+++ realestate-site/frontend/config.js
@@ -1,7 +1,2 @@
-// این دو خط رو با اطلاعات خودت جایگزین کن
-
-// آدرس API که روی Render بالا آوردی (مرحله بعدی در راهنما)
-const API_BASE_URL = "https://your-backend-name.onrender.com";
-
-// یوزرنیم ربات تلگرامت (بدون @)
-const TELEGRAM_BOT_USERNAME = "your_bot_username";
+const API_BASE_URL = "https://real-estate-site-svyr.onrender.com";
+const TELEGRAM_BOT_USERNAME = "KhademAbad_RealEstate_bot";
