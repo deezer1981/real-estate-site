91f3eb5215aec40c1bcfff32364e2395bab20e60
deezer1981 <145301005+deezer1981@users.noreply.github.com>
2026-08-10 00:17:38 +0330

    Update config.js

--- realestate-site/frontend/config.js
+++ realestate-site/frontend/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://api.atlas-amlak.ir";
+const API_BASE_URL = "https://real-estate-site-svyr.onrender.com";
 const TELEGRAM_BOT_USERNAME = "KhademAbad_RealEstate_bot";
 const WHATSAPP_NUMBER = "989106943220";
 const RUBIKA_USERNAME = "Nobody_Mohsen";
