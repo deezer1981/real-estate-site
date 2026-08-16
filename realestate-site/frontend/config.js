7b7592fe5f8c99362f6aa3f91ef314856671c3ea
deezer1981 <145301005+deezer1981@users.noreply.github.com>
2026-08-16 21:49:58 +0330

    Add files via upload

---
+++ realestate-site/frontend/config.js
@@ -0,0 +1,4 @@
+// تنظیمات سایت اطلس املاک
+const WHATSAPP_NUMBER = "989106943220";
+const RUBIKA_USERNAME = "Nobody_Mohsen";
+const BALE_USERNAME = "Nobody_Mohsen"; // اگر آیدی بله متفاوت است عوض کن
