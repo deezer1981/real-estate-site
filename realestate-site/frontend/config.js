9fc601d2dc59c10cea5073bc19012924f3f296fa
deezer1981 <145301005+deezer1981@users.noreply.github.com>
2026-08-16 21:49:10 +0330

    Delete realestate-site/frontend/config.js

--- realestate-site/frontend/config.js
+++
@@ -1,4 +0,0 @@
-const API_BASE_URL = "https://api.atlas-amlak.ir";
-const TELEGRAM_BOT_USERNAME = "KhademAbad_RealEstate_bot";
-const WHATSAPP_NUMBER = "989106943220";
-const RUBIKA_USERNAME = "Nobody_Mohsen";
