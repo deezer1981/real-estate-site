ca8725b9fad622137ee8ab5e586d7b47a5d78ae5
deezer1981 <145301005+deezer1981@users.noreply.github.com>
2026-08-17 06:46:21 +0330

    Delete realestate-site/frontend/config.js

--- realestate-site/frontend/config.js
+++
@@ -1,5 +0,0 @@
-// تنظیمات سایت اطلس املاک
-const WHATSAPP_NUMBER = "989106943220";
-const OFFICE_PHONE = "09106943220";
-const RUBIKA_USERNAME = "Nobody_Mohsen";
-const BALE_USERNAME = "Nobody_Mohsen";
